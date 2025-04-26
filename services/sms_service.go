215bcb4818d66858925f661b4375a3215f61f2f8
lakshya1goel <lakshya1234goel@gmail.com>
2025-04-26 22:31:02 +0530

    add sms service

--- services/sms_service.go
+++ services/sms_service.go
@@ -1,8 +1,25 @@
 package services
 
-func SendSms(mobile string, message string, otp string) error {
-	// Implement the logic to send SMS using a third-party service
-	// For example, using Twilio or any other SMS gateway
-	// This is just a placeholder implementation
+import (
+	"fmt"
+	"os"
+
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+func SendSms(to string, otp string) error {
+	client := twilio.NewRestClient()
+	params := &openapi.CreateMessageParams{}
+	params.SetTo(to)
+	params.SetFrom(os.Getenv("TWILIO_PHONE_NUMBER"))
+	params.SetBody("This is your OTP: " + otp)
+
+	_, err := client.Api.CreateMessage(params)
+	if err != nil {
+		fmt.Println(err.Error())
+		return err
+	} 
+	fmt.Println("SMS sent successfully!")
 	return nil
 }
